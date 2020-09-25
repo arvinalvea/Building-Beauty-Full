b0ec0a4b119735bb6e87749b3b4664a920357c30
Arvin Alvea <arvinalvea@gmail.com>
2020-09-25 13:16:23 -0400

    Styled nav bar and fixed burger

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  //Toggle Nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+// const nav = document.querySelector("nav");
+// const sectionOne = document.querySelector(".hero-text h1");
+
+// const sectionOneOptions = {
+//   rootMargin: "-257px 0px 0px 0px",
+// };
+
+// const sectionOneObserver = new IntersectionObserver(function (
+//   entries,
+//   sectionOneObserver
+// ) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       nav.classList.add("nav-scrolled");
+//     } else {
+//       nav.classList.remove("nav-scrolled");
+//     }
+//   });
+// },
+// sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
+
+navSlide();
