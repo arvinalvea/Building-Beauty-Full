a26e99a13ff13dc8fb8b6e49ac6658d7a4353f40
Arvin Alvea <arvinalvea@gmail.com>
2020-10-05 13:05:42 -0400

    Added new font weights to about.html

---
+++ js/about.js
@@ -0,0 +1,60 @@
+const nav = document.querySelector("nav");
+const sectionOne = document.querySelector(".main-about h1");
+
+const sectionOneOptions = {
+  rootMargin: "-100px 0px 0px 0px",
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      nav.classList.add("nav-scrolled");
+    } else {
+      nav.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  //Toggle Nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+// Smooth scroll when arrow button is clicked
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+sectionOneObserver.observe(sectionOne);
+
+navSlide();
